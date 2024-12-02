bda524d37262c9c6acf6ef264a8dd79808a1c638
Sadman Hafij <141361791+sadman119@users.noreply.github.com>
2024-12-03 00:14:18 +0600

    Create Bublesort.cpp

---
+++ Bublesort.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Function for Bubble Sort
+void bubbleSort(vector<int>& arr) {
+    for (size_t i = 0; i < arr.size() - 1; i++) {
+        for (size_t j = 0; j < arr.size() - i - 1; j++) {
+            if (arr[j] > arr[j + 1])
+                swap(arr[j], arr[j + 1]);
+        }
+    }
+}
+
+int main() {
+    vector<int> arr = {5, 1, 4, 2, 8};
+    bubbleSort(arr);
+    cout << "Sorted array: ";
+    for (int num : arr)
+        cout << num << " ";
+    return 0;
+}
