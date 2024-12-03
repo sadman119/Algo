83fe94de76460278ed16676db131f5392c28f6ef
Sadman Hafij <141361791+sadman119@users.noreply.github.com>
2024-12-03 07:52:07 +0600

    Update InsertionSort.c

--- InsertionSort.c
+++ InsertionSort.c
@@ -3,15 +3,17 @@
 // Function for Insertion Sort
 void insertionSort(int arr[], int n) {
     for (int i = 1; i < n; i++) {
-        int key = arr[i];
-        int j = i - 1;
-
-        // Move elements of arr[0..i-1] that are greater than key, one position ahead
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j--;
+        for (int j = i; j > 0; j--) {
+            // Swap if the current element is smaller than the previous one
+            if (arr[j] < arr[j - 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+            } else {
+                // If no swap needed, break out of the inner loop
+                break;
+            }
         }
-        arr[j + 1] = key;
     }
 }
 
